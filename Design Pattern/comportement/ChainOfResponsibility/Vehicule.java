c47e5c8694541bd4c4222ab6b6c4e8e662c4afec
Stephane <stephane_hotmail@hotmail.com>
2017-04-24 16:38:36 +0200

    design pattern + architectures

---
+++ Design Pattern/comportement/ChainOfResponsibility/Vehicule.java
@@ -0,0 +1,14 @@
+public class Vehicule extends ObjetBase
+{
+  protected String description;
+
+  public Vehicule(String description)
+  {
+    this.description = description;
+  }
+
+  protected String description()
+  {
+    return description;
+  }
+}
